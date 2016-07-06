a23ce3512ee5a886a3d3d8a35bb628784755e2b9
Barthelemy Dagenais <bart@resulto.ca>
2016-07-06 09:32:38 -0400

    Initial work on binary transfer performance between python and java

---
+++ java/src/com/infobart/bytesperf/Application.java
@@ -0,0 +1,141 @@
+package com.infobart.bytesperf;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Base64;
+import java.util.DoubleSummaryStatistics;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Application {
+
+	public static Map<Integer, DoubleSummaryStatistics> stats = new HashMap<Integer, DoubleSummaryStatistics>();
+
+	public static void readStream(Socket socket, boolean base64) throws Exception {
+		DataInputStream input = new DataInputStream(socket.getInputStream());
+		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		Base64.Decoder decoder = Base64.getDecoder();
+
+		long startNano = System.nanoTime();
+		int size = input.readInt();
+		byte[] data = new byte[size];
+		input.readFully(data);
+//		System.out.println(size);
+//		System.out.println((int)data[0]);
+//		System.out.println((int)data[size-1]);
+		if (base64) {
+			data = decoder.decode(data);
+		}
+		output.writeByte(0);
+		output.flush();
+		long stopNano = System.nanoTime();
+		long timeNs = stopNano - startNano;
+		double timeMs = (double)timeNs / 1000.0 / 1000.0;
+		//System.out.println("Read fully " + (int)data[0] + " to " + (int)data[size-1] + " in " + (stop-start));
+//		System.out.println("Read " + size + " bytes in " + timeMs + " ms or " + timeNs + " "
+//				+ "ns");
+
+		DoubleSummaryStatistics currentStats = stats.get(size);
+		if (currentStats == null) {
+			currentStats = new DoubleSummaryStatistics();
+			stats.put(size, currentStats);
+		}
+		currentStats.accept(timeNs);
+		System.out.println("MS: " + timeMs + ". Average NS for size " + size + ": " + currentStats.getAverage());
+	}
+
+	public static void readChannel(Socket socket, boolean direct, boolean base64) throws Exception {
+		DataInputStream input = new DataInputStream(socket.getInputStream());
+		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		ReadableByteChannel readChannel = Channels.newChannel(socket.getInputStream());
+		Base64.Decoder decoder = Base64.getDecoder();
+		ByteBuffer buffer;
+		int totalSize = 0;
+		byte[] data;
+//		int reads = 0;
+
+		long startNano = System.nanoTime();
+		long start = System.currentTimeMillis();
+		int size = input.readInt();
+		if (direct) {
+			buffer = ByteBuffer.allocateDirect(size);
+		} else {
+			buffer = ByteBuffer.allocate(size);
+		}
+		while (totalSize < size) {
+			totalSize += readChannel.read(buffer);
+//			reads++;
+		}
+
+		buffer.flip();
+		if (buffer.hasArray()) {
+			data = buffer.array();
+		} else {
+			data = new byte[size];
+			buffer.get(data);
+		}
+		if (base64) {
+			data = decoder.decode(data);
+		}
+		output.writeByte(0);
+		output.flush();
+		long stop = System.currentTimeMillis();
+		long stopNano = System.nanoTime();
+		long timeMs = stop - start;
+		long timeNs = stopNano - startNano;
+//		buffer.flip();
+//		System.out.println((int)buffer.get(0));
+//		System.out.println((int)buffer.get(size-1));
+
+		DoubleSummaryStatistics currentStats = stats.get(size);
+		if (currentStats == null) {
+			currentStats = new DoubleSummaryStatistics();
+			stats.put(size, currentStats);
+		}
+		currentStats.accept(timeNs);
+		System.out.println("MS: " + timeMs + ". Average NS for size " + size + ": " + currentStats.getAverage());
+//		System.out.println("Reads: " + reads);
+
+	}
+
+	public static void receive(Socket socket) {
+		try {
+//			readStream(socket, true);
+			readChannel(socket, false, true);
+			socket.close();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void processSocket(final Socket socket) {
+		new Thread(new Runnable() {
+			@Override public void run() {
+				receive(socket);
+			}
+		}).start();
+	}
+
+	public static void listen() {
+		try {
+			ServerSocket sSocket = new ServerSocket(10000);
+			while (true) {
+				Socket socket = sSocket.accept();
+				processSocket(socket);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		listen();
+	}
+}
---
+++ python/bytesperf.py
@@ -0,0 +1,89 @@
+import base64
+from collections import defaultdict
+import os
+import socket
+from statistics import mean
+import time
+
+
+SMALL = 4
+
+MEDIUM = 1024
+
+LARGE = 1024 * 512
+
+EXTRA_LARGE = 1024 * 1024 * 64
+
+EXTRA_EXTRA_LARGE = 1024 * 1024 * 1024
+
+
+def send(sock, data, size):
+    totalsent = 0
+    while totalsent < size:
+        sent = sock.send(data[totalsent:])
+        if sent == 0:
+            raise RuntimeError("socket connection broken")
+        totalsent = totalsent + sent
+
+
+def receive(sock):
+    response = sock.recv(1)
+    # print(response)
+    return response
+
+
+def get_int(a_byte):
+    return int.from_bytes(a_byte, "big", signed=True)
+
+
+def transfer(size, stats, use_base64=False):
+    if size <= 1024:
+        to_transfer = os.urandom(size)
+    else:
+        # urandom is very slow for very large number
+        to_transfer = os.urandom(1024) * (size // 1024)
+    print("{0} to {1}".format(
+        get_int(to_transfer[0:1]), get_int(to_transfer[-1:])))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('127.0.0.1', 10000)
+    sock.connect(server_address)
+    time.sleep(0.5)
+    start = time.time()
+    if use_base64:
+        to_transfer = base64.b64encode(to_transfer)
+        size = len(to_transfer)
+        # print(to_transfer)
+    # print("{0} to {1}".format(
+        # get_int(to_transfer[0:1]), get_int(to_transfer[-1:])))
+    send(sock, size.to_bytes(4, "big", signed=True), 4)
+    send(sock, to_transfer, size)
+    receive(sock)
+    stop = time.time()
+    timeSec = stop - start
+    stats[size].append(timeSec * 1000 * 1000 * 1000)
+    try:
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+    except Exception:
+        pass
+    print("Took {0} to send {1} bytes. Average: {2}".format(
+        timeSec * 1000, size, mean(stats[size])))
+
+
+def main():
+    base64 = True
+    stats = defaultdict(list)
+    for i in range(10):
+        transfer(SMALL, stats, base64)
+        time.sleep(1)
+        transfer(MEDIUM, stats, base64)
+        time.sleep(1)
+        transfer(LARGE, stats, base64)
+        time.sleep(1)
+        transfer(EXTRA_LARGE, stats, base64)
+        time.sleep(1)
+        transfer(EXTRA_EXTRA_LARGE, stats, base64)
+
+
+if __name__ == "__main__":
+    main()
